fe71cc3a27f2df181b9cf0c25669ee273bdebab8
kazimunshimun <anik1.bd@gmail.com>
2016-06-15 10:57:13 +0600

    iOS project initial commit

---
+++ iOS/mSpeechSimulator/mSpeechSimulator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mSpeechSimulator
+//
+//  Created by Anik on 6/15/16.
+//  Copyright © 2016 mStudoApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/mSpeechSimulator/mSpeechSimulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mSpeechSimulator
+//
+//  Created by Anik on 6/15/16.
+//  Copyright © 2016 mStudoApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
